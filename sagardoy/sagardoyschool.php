8e210649e2a7be3d98b04fdeb1f7c92875ae9d11
Cristo <cristo.ruiz@hivent.es>
2024-11-13 01:59:52 +0100

    get template ok

--- sagardoy/sagardoyschool.php
+++ sagardoy/sagardoyschool.php
@@ -9,22 +9,22 @@ get_header();
 ?>
 
 <!-- Quienes Cabecera-->
-<?php get_template_part('componentes/generales/comp','cabecera');?>
+<?php get_template_part('componentes/sagardoyschool/comp','cabecera');?>
 
 <!-- Quienes Areas -->
-<?php get_template_part('componentes/generales/comp','areas');?>
+<?php get_template_part('componentes/sagardoyschool/comp','areas');?>
 
 <!-- Quienes Open Company -->
-<?php get_template_part('componentes/generales/comp','opencompany');?>
+<?php get_template_part('componentes/sagardoyschool/comp','opencompany');?>
 
 <!-- Quienes Futuro -->
-<?php get_template_part('componentes/generales/comp','futuro');?>
+<?php get_template_part('componentes/sagardoyschool/comp','futuro');?>
 
 <!-- Quienes opiniones OK -->
 <?php get_template_part('componentes/generales/comp','opiniones');?>
 
 <!-- Quienes Alturas -->
-<?php get_template_part('componentes/generales/comp','alturas');?>
+<?php get_template_part('componentes/sagardoyschool/comp','alturas');?>
 
 
   
