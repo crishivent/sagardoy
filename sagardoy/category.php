cd697314882f9013156e9f670178f82b966ef8f2
Cristo <cristo.ruiz@hivent.es>
2024-12-06 11:18:13 +0100

    modificacion categoria

--- sagardoy/category.php
+++ sagardoy/category.php
@@ -8,8 +8,117 @@ get_header();
 
 ?>
 
+<?php get_template_part('componentes/category/comp','cabecera');?>
+
+<!-- post de la categoria -->
+<section class="modulo-20 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
 
 
+
+
+       <?php 
+            // Obtener las categorías del post actual
+                $categories = wp_get_post_categories( get_the_ID() );
+
+                if ( $categories ) {
+                    // Crear consulta para posts relacionados
+                    $args = array(
+                        'category__in'   => $categories,     // Coincidir con las categorías del post actual
+                        'post__not_in'   => array( get_the_ID() ), // Excluir el post actual
+                        'posts_per_page' => 20,               // Número de posts relacionados a mostrar
+                        'orderby'        => 'rand',
+                        'ignore_sticky_posts' => 1           // Ignorar posts fijos
+                    );
+                    $related_posts = new WP_Query( $args );
+
+                    // Verificar si hay posts relacionados
+                    if ( $related_posts->have_posts() ) {
+                        while ( $related_posts->have_posts() ) {
+                            $related_posts->the_post();
+                            $img=$thumbnail_url = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );
+                             $post_categories = get_the_category();
+                             $category_name = !empty($post_categories) ? $post_categories[0]->name : 'Sin categoría';
+
+                             // Obtener la fecha de publicación
+                             $post_date = get_the_date('d.m.Y'); // Formato: día.mes.año
+                            ?>
+
+                             <div class="col-12 col-sm-6 col-lg-4 mb-4">
+                                <a href="#">
+                                    <div class="line-border">
+                                    <div class="row">
+                                        <div class="col-5 col-sm-4 col-md-4">
+                                        <img src="<img src="<?php echo $img; ?>" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="col-7 col-sm-8 col-md-8">
+                                        <div class="data">
+                                            <div class="d-flex flex-column justify-content-between">
+                                            <div>
+                                                <h2 class="subtitulo"><?php echo esc_html($category_name); ?></h2>
+                                                <h2 class="titulo"><?php the_title(); ?></h2>
+                                            </div>
+                                            <div>
+                                                <div class="fecha"><?php echo esc_html($post_date); ?></div>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <?php
+                        }
+                    
+                    } else {
+                        echo '<p>No hay posts.</p>';
+                    }
+
+                    // Restaurar postdata original después de la consulta personalizada
+                    wp_reset_postdata();
+                }
+            
+            
+            ?>
+
+      </div>
+    </div>
+  </section>
+<!-- fin post de la categoria -->
+
+            <!-- otras publicaciones -->
+  <section class="modulo-21 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-6">
+          <h2 class="titulo"><?php _e('Otras publicaciones', 'sagardoy'); ?></h2>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6">
+          <div class="btns">
+               <?php
+                    // Obtener solo las 3 primeras categorías
+                    $categories = get_categories(array(
+                        'number' => 3, // Mostrar solo 3 categorías
+                    ));
+
+                    // Iterar a través de las categorías seleccionadas y generar enlaces
+                    foreach ($categories as $category) {
+                        // Obtener el enlace de la categoría
+                        $category_link = get_category_link($category->term_id);
+                        ?>
+                        <a href="<?php echo esc_url($category_link); ?>" class="btn-link">
+                            <?php echo esc_html($category->name); ?>
+                        </a>
+                        <?php
+                    }
+                    ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
 
---
+++ sagardoy/componentes/category/comp-cabecera.php
@@ -0,0 +1,21 @@
+
+  <section class="section cabecera-interna-publicaciones cabecera">
+    <a href="#" class="btn-volver"><u>Publicaciones</u></a>
+
+    <div class="holder">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-lg-8">
+            <div class="titulo">Sagardoy Legal News</div>
+          </div>
+          <div class="col-12 col-sm-12 col-lg-4"></div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-lg-6">
+            <p class="texto">Facilisi vitae suspendisse eget dui donec. Ac quisque quis in quis integer volutpat. In tellus commodo amet cursus dolor vitae leo. Amet etiam at nisl velit volutpat arcu volutpat.</p>
+          </div>
+          <div class="col-12 col-sm-12 col-lg-6"></div>
+        </div>
+      </div>
+    </div>
+  </section>
