3425b28e64456b08602e4fb126840f34505f6e85
Cristo <cristo.ruiz@hivent.es>
2024-12-07 00:46:10 +0100

    resultados y search

---
+++ sagardoy/publicaciones.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Publicaciones
+*/
+
+get_header();
+
+
+ ?>
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ sagardoy/search.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Página de Busqueda
+*/
+
+get_header();
+
+
+ ?>
+
+<h1>Resultados de busqueda</h1>
+
+
+
+
+<?php get_footer(); ?>
