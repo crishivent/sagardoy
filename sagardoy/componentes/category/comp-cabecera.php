337ca5e7f5522c4bc79e632731d22fed601f73f9
Cristo <cristo.ruiz@hivent.es>
2024-12-06 11:30:30 +0100

    cabecera categoria

--- sagardoy/componentes/category/comp-cabecera.php
+++ sagardoy/componentes/category/comp-cabecera.php
@@ -1,21 +1,21 @@
-
-  <section class="section cabecera-interna-publicaciones cabecera">
-    <a href="#" class="btn-volver"><u>Publicaciones</u></a>
-
+<section class="section cabecera-interna-publicaciones cabecera">
+    <a href="https://sagardoy.neuronalcode.io/blog/" class="btn-volver"><u><?php _e('Publicaciones', 'sagardoy'); ?></u></a>
     <div class="holder">
       <div class="container">
         <div class="row">
           <div class="col-12 col-sm-12 col-lg-8">
-            <div class="titulo">Sagardoy Legal News</div>
+            <!-- Título de la categoría actual -->
+            <div class="titulo"><?php single_cat_title(); ?></div>
           </div>
           <div class="col-12 col-sm-12 col-lg-4"></div>
         </div>
         <div class="row">
           <div class="col-12 col-sm-12 col-lg-6">
-            <p class="texto">Facilisi vitae suspendisse eget dui donec. Ac quisque quis in quis integer volutpat. In tellus commodo amet cursus dolor vitae leo. Amet etiam at nisl velit volutpat arcu volutpat.</p>
+            <!-- Descripción de la categoría actual -->
+            <p class="texto"><?php echo category_description(); ?></p>
           </div>
           <div class="col-12 col-sm-12 col-lg-6"></div>
         </div>
       </div>
     </div>
-  </section>
+</section>
