1d5da6751ebe0faf762ae5d3f4d398e04c12e50f
Cristo <cristo.ruiz@hivent.es>
2024-12-07 21:26:38 +0100

    alturas con lógica

--- sagardoy/componentes/sagardoyschool/comp-alturas.php
+++ sagardoy/componentes/sagardoyschool/comp-alturas.php
@@ -1,19 +1,29 @@
+<?php 
+$imagen_alturas=get_query_var("imagen_alturas");
+$titulo_alturas=get_query_var("titulo_alturas");
+$enlace_alturas=get_query_var("enlace_alturas");
+$direccion_alturas=get_query_var("direccion_alturas");
+$email_alturas=get_query_var("email_alturas");
+$telefono_alturas=get_query_var("telefono_alturas");
+$enlace_maps_alturas=get_query_var("enlace_maps_alturas");
+?>
+
 <section class="modulo-34 pt-130 pb-130">
     <div class="container">
       <div class="row">
         <div class="col-12 col-sm-12 col-lg-5">
-          <img src="images/img-alcanza.jpg" class="img-fluid" alt="" />
+          <img src="<?php echo $imagen_alturas; ?>" class="img-fluid" alt="" />
         </div>
         <div class="col-12 col-sm-12 col-lg-1"></div>
         <div class="col-12 col-sm-12 col-lg-5 d-flex justify-content-between flex-column gap-3">
           <div>
-            <h2 class="titulo">Alcanza nuevas alturas en tu carrera</h2>
-            <a href="#" class="btn-link">Acceder a Sagardoy School</a>
+            <h2 class="titulo"><?php echo $titulo_alturas;?></h2>
+            <a href="<?php echo $enlace_alturas; ?>" class="btn-link"><?php _e('Acceder a Sagardoy School', 'sagardoy'); ?></a>
           </div>
           <div class="info">
-            <a href="#" class="btn-direccion">C/Velázquez 86D.<br>28006 Madrid</a><br>
-            <a href="mailto:info@sagardoyschool.com" class="btn-email">info@sagardoyschool.com</a><br>
-            <a href="tel:+34914540071">+34 91 454 0071</a>
+            <a href="<?php echo $enlace_maps_alturas ?>" class="btn-direccion" target="_blank"><?php echo $direccion_alturas;?></a><br>
+            <a href="mailto:<?php echo $email_alturas;?>" class="btn-email"><?php echo $email_alturas;?></a><br>
+            <a href="tel:<?php echo $telefono_alturas;?>"><?php echo $telefono_alturas;?></a>
           </div>
         </div>
         <div class="col-12 col-sm-12 col-lg-1"></div>
--- sagardoy/sagardoyschool.php
+++ sagardoy/sagardoyschool.php
@@ -30,6 +30,14 @@ set_query_var("lista_areas", get_field('lista_areas'));
 set_query_var("open_company", get_field('open_company'));
 set_query_var("in_company", get_field('in_company'));
 
+// Alturas
+set_query_var("imagen_alturas", get_field('imagen_alturas'));
+set_query_var("titulo_alturas", get_field('titulo_alturas'));
+set_query_var("enlace_alturas", get_field('enlace_alturas'));
+set_query_var("direccion_alturas", get_field('direccion_alturas'));
+set_query_var("email_alturas", get_field('email_alturas'));
+set_query_var("telefono_alturas", get_field('telefono_alturas'));
+set_query_var("enlace_maps_alturas", get_field('enlace_maps_alturas'));
 
 
 
@@ -41,7 +49,7 @@ set_query_var("in_company", get_field('in_company'));
 <!-- Quienes Areas Ok-->
 <?php get_template_part('componentes/sagardoyschool/comp','areas');?>
 
-<!-- Quienes Open Company -->
+<!-- Quienes Open Company Ok-->
 <?php get_template_part('componentes/sagardoyschool/comp','opencompany');?>
 
 <!-- Quienes Futuro OK-->
