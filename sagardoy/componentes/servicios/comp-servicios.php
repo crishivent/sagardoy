129aa3396061f0d64d701f118452b09cb9c05739
Cristo <cristo.ruiz@hivent.es>
2024-12-07 18:54:12 +0100

    comp-servicios eliminacion div extra

--- sagardoy/componentes/servicios/comp-servicios.php
+++ sagardoy/componentes/servicios/comp-servicios.php
@@ -8,31 +8,31 @@ $servicios = get_query_var('servicios');
       <div class="row">
         <div class="col-12 col-sm-12 col-lg-3">
           <h2 class="titulo"><?php _e('Servicios Sagardoy', 'sagardoy'); ?></h2>
-          <ul class="lista">
+            <ul class="lista">
 
-          <?php 
-              $a=1;
-              foreach ($servicios as $elemento) {
-                $titulo = $elemento['elementos_servicio']['titulo_servicio'];
-                  if($a==1){$acti="activo";}else{$acti='';}
-                  echo '<li><a href="#" data-id="0'.$a.'" class="btn-link '.$acti.'">'.$titulo.'</a></li>';
-                  $a++;
-              }
-            ?>
-          </ul>
-          <div class="select">
-            <select id="select">
-              <option value="0" disabled selected><?php _e('Seleccionar...', 'sagardoy'); ?></option>
-              <?php 
-              $b=1;
-              foreach ($servicios as $elemento) {
-                $titulo = $elemento['elementos_servicio']['titulo_servicio'];
-                  echo '<option value="0'.$b.'">'.$titulo.'</option>';
-                  $b++;
-              }
-            ?>
-            </select>
-          </div>
+            <?php 
+                $a=1;
+                foreach ($servicios as $elemento) {
+                    $titulo = $elemento['elementos_servicio']['titulo_servicio'];
+                    if($a==1){$acti="activo";}else{$acti='';}
+                    echo '<li><a href="#" data-id="0'.$a.'" class="btn-link '.$acti.'">'.$titulo.'</a></li>';
+                    $a++;
+                }
+                ?>
+            </ul>
+            <div class="select">
+                <select id="select">
+                <option value="0" disabled selected><?php _e('Seleccionar...', 'sagardoy'); ?></option>
+                <?php 
+                $b=1;
+                foreach ($servicios as $elemento) {
+                    $titulo = $elemento['elementos_servicio']['titulo_servicio'];
+                    echo '<option value="0'.$b.'">'.$titulo.'</option>';
+                    $b++;
+                }
+                ?>
+                </select>
+            </div>
         </div>
         <div class="col-12 col-sm-12 col-lg-1"></div>
         <div class="col-12 col-sm-12 col-lg-8">
@@ -57,8 +57,7 @@ $servicios = get_query_var('servicios');
                                     </div>
                                     <div class="col-12 col-sm-12 col-lg-2"></div>
                                 </div>
-                            </div>
-                        </div>';
+                            </div>';
                   $c++;
               }
             ?>
