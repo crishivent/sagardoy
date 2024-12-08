e5fe569268b27ff76a0b3815aa2389d0ac9bc28c
Cristo <cristo.ruiz@hivent.es>
2024-12-08 23:05:08 +0100

    generica

---
+++ sagardoy/generica.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Página generica
+*/
+
+get_header();
+
+?>
+
+<section class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-8 col-md-8">
+          <?php the_content();?>
+            
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php get_footer(); ?>
