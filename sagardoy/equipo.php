1689d58a7b52ef4ed73ae95453bba84915203095
Cristo <cristo.ruiz@hivent.es>
2024-12-08 22:22:00 +0100

    single equipo

--- sagardoy/equipo.php
+++ sagardoy/equipo.php
@@ -9,6 +9,198 @@ get_header();
 
 
 
+<!-- Equipo Cabecera -->
+<section class="section cabecera-interna cabecera">
+    <div class="holder">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-7 col-md-7 border-left">
+            <div class="label">Equipo</div>
+            <div class="titulo">Una larga trayectoria profesional</div>
+          </div>
+          <div class="col-12 col-sm-5 col-md-5"></div>
+        </div>
+      </div>
+    </div>
+    <img src="images/img-cabecera-equipo.jpg" class="img-fluid" alt="" />
+  </section>
+
+
+
+<!-- Contador gris -->
+<section class="modulo-03 pt-90 pb-90 bg-gris">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-7 col-md-7">
+          <div class="texto-top">Quam dui montes quam ultrices aliquet mollis ornare amet. Viverra phasellus sit integer proin faucibus. Ut nisl dictumst odio neque. Odio feugiat scelerisque non ac. Urna netus pellentesque nulla elementum. Turpis dictum vulputate bibendum velit sagittis. Purus nulla.</div>
+        </div>
+        <div class="col-12 col-sm-5 col-md-5"></Div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-sm-3 col-md-3 text-center">
+          <div class="holder">
+            <div class="numero"><span class="plus">+</span><span class="counter" data-count="43">0</span></div>
+            <div class="texto">años de experiencia</div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-3 col-md-3 text-center">
+          <div class="holder">
+            <div class="numero"><span class="plus">+</span><span class="counter" data-count="350">0</span></div>
+            <div class="texto">profesionales</div>
+            </div>
+        </div>
+        <div class="col-12 col-sm-3 col-md-3 text-center">
+          <div class="holder">
+            <div class="numero"><span class="counter" data-count="35">0</span></div>
+            <div class="texto">sedes en el mundo</div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-3 col-md-3 text-center">
+          <div class="holder">
+            <div class="numero"><span class="plus">+</span><span class="counter" data-count="43">0</span></div>
+            <div class="texto">años de experiencia</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<!-- Filtros profesionales -->
+
+<section class="modulo-11 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-lg-1"></div>
+        <div class="col-12 col-sm-12 col-lg-10 text-center">
+          <h2 class="titulo">Buscar abogados y profesionales</h2>
+          <div class="form">
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4">
+                <div class="input-email">
+                  <input class="input" value="" placeholder="Buscar"></input>
+                  <a href="#" class="btn-link"></a>
+                </div>
+              </div>
+              <div class="col-12 col-sm-3 col-md-3">
+                <div class="select">
+                  <select id="standard-select">
+                    <option value="0" disabled selected>Cargo</option>
+                    <option value="1">Opción 1</option>
+                    <option value="2">Opción 2</option>
+                    <option value="3">Opción 3</option>
+                    <option value="4">Opción 4</option>
+                    <option value="5">Opción 5</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-12 col-sm-3 col-md-3">
+                <div class="select">
+                  <select id="standard-select">
+                    <option value="0" disabled selected>Sede</option>
+                    <option value="1">Opción 1</option>
+                    <option value="2">Opción 2</option>
+                    <option value="3">Opción 3</option>
+                    <option value="4">Opción 4</option>
+                    <option value="5">Opción 5</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-12 col-sm-2 col-md-2">
+                <a href="#" class="btn-buscar">Buscar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-1"></div>
+      </div>
+    </div>
+  </section>
+
+
+<!-- Resultado -->
+ <section class="modulo-11 pt-130 pb-130 buscador-resultado">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color1.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Iñigo Sagardoy de Simón</div>
+          <div class="cargo">Presidente</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color2.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Juan Antonio Sagardoy Bengoechea</div>
+          <div class="cargo">Presidente de Honor</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color3.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Martín Godino Reyes</div>
+          <div class="cargo">Socio Director</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color4.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Ana Godino Reyes</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color5.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Román Gil Alburquerque</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color6.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Raquel Muñiz Ferrer</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color7.jpg" class="img-fluid" alt="" />
+          <div class="nombre">José Luis Fraile Quinzaños</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color8.jpg" class="img-fluid" alt="" />
+          <div class="nombre">María Jesús Herrera Duque</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color9.jpg" class="img-fluid" alt="" />
+          <div class="nombre">David Isaac Tobía</div>
+          <div class="cargo">Socio</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color10.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Montserrat Alonso Paulí</div>
+          <div class="cargo">Socia</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color11.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Bruno Álvarez Padín</div>
+          <div class="cargo">Socio</div>
+        </div>
+        <div class="col-12 col-sm-4 col-lg-3 mb-5">
+          <img src="images/I_igo_Sagardoy_de_Sim_n-1-color12.jpg" class="img-fluid" alt="" />
+          <div class="nombre">Marc Carrera Domènech</div>
+          <div class="cargo">Socio</div>
+        </div>
+      </div>
+      <div class="text-center"><a href="#" class="btn-ver-mas">Ver más</a></div>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ sagardoy/single-equipo.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Single Equipo
+*/
+
+get_header();
+
+$cargo= get_field('cargo');
+$formacion= get_field('formacion');
+$menciones= get_field('menciones');
+$idiomas= get_field('idiomas');
+$email= get_field('email');
+$telefono= get_field('telefono');
+$imagen_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+
+
+
+?>
+
+<!-- Cabecera -->
+<section class="section cabecera-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-8 col-md-8">
+          <div class="datos">
+            <div class="nombre"><?php the_title();?></div>
+            <div class="cargo"><?php echo $cargo;?></div>
+            <img src="<?php echo $imagen_url; ?>" class="img-fluid mobile" alt="" />
+            <div class="row">
+              <div class="col-12 col-sm-4 col-md-4">
+                <div class="titulo-top"><?php _e('CONTACTO', 'sagardoy'); ?></div>
+                <a href="mailto:<?php echo $email; ?>" class="btn-email"><?php echo $email; ?></a>
+                <a href="tel:<?php echo $telefono; ?>" class="btn-telefono"><?php echo $telefono;?></a>
+              </div>
+              <div class="col-12 col-sm-4 col-md-4">
+                <div class="titulo-top"><?php _e('SEDES', 'sagardoy'); ?></div>
+                <div class="texto">Sagardoy Madrid</div>
+              </div>
+              <div class="col-12 col-sm-4 col-md-4">
+                <div class="titulo-top"><?php _e('IDIOMAS', 'sagardoy'); ?></div>
+                <div class="texto"><?php echo $idiomas;?></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-1 col-md-1"></div>
+        <div class="col-12 col-sm-3 col-md-3">
+          <img src="<?php echo $imagen_url; ?>" class="img-fluid" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<!-- Formación y menciones -->
+ <section class="modulo-22 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-6">
+          <h2 class="titulo"><?php _e('Presentación', 'sagardoy'); ?></h2>
+          <p class="texto">
+            <?php the_content();?>
+          </p>
+        </div>
+        <div class="col-12 col-sm-1 col-md-1"></div>
+        <div class="col-12 col-sm-4 col-md-4">
+          <div class="info">
+            <h2 class="titulo"><?php _e('Formación', 'sagardoy'); ?></h2>
+            <div class="datos">
+              <?php echo $formacion;?>
+            </div>
+            <div class="ico-mas"></div>
+          </div>
+          <div class="info">
+            <h2 class="titulo"><?php _e('Menciones', 'sagardoy'); ?></h2>
+            <div class="datos">
+              <?php echo $menciones; ?>
+            </div>
+            <div class="ico-mas"></div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-1 col-md-1"></div>
+      </div>
+    </div>
+  </section>
+
+
+
+
+
+<?php get_footer(); ?>
