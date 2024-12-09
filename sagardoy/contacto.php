58175b886c92a8ccb1f86a88d00e05c96171ae8c
Cristo <cristo.ruiz@hivent.es>
2024-12-09 13:22:36 +0100

    pagina contacto

--- sagardoy/contacto.php
+++ sagardoy/contacto.php
@@ -5,9 +5,19 @@ Template Name: Página de contacto
 
 get_header();
 
-$sedes = get_field('sedes', 'option');
 $email = get_field('email_general', 'option');
 $telefono = get_field('telefono_general', 'option');
+
+
+
+$email = get_field('email');
+$telefono = get_field('telefono');
+$direccion = get_field('direccion');
+$descripcion = get_field('descripcion');
+$titulo_derecha = get_field('titulo_derecha');
+$imagen_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+
+
 
  ?>
 
@@ -19,15 +29,44 @@ $telefono = get_field('telefono_general', 'option');
             <h2 class="titulo"><?php _e('Contacto', 'sagardoy'); ?></h2>
             <div class="subtitulo"><?php _e('Nuestras Sedes', 'sagardoy'); ?></div>
             <ul class="lista">
-                <?php 
-                $i=1;
-                foreach ($sedes as $sede) {
-                    if($i==1){$activo='activo';}else{$activo='';}
-                    echo '<li><a href="#" data-id="0'.$i.'" class="btn-link '.$activo.'">'.htmlspecialchars($sede['nombre']).'</a></li>';
-                    $i++;
-                }?>
-              
+                <?php
+                // Realiza una consulta personalizada para obtener los posts del custom post type 'sedes'
+                $args = array(
+                    'post_type' => 'sede', // Nombre del custom post type
+                    'posts_per_page' => -1, // Trae todos los posts
+                    'post_status' => 'publish', // Solo los publicados
+                );
+                $current_id = get_the_ID();
+                $query = new WP_Query($args);
+                $i = 1; // Contador para identificar el índice
+
+                if ($query->have_posts()) :
+                    while ($query->have_posts()) : $query->the_post();
+                    $activo = (get_the_ID() == $current_id) ? 'activo' : '';
+                        ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>" data-id="02" class="btn-link <?php echo $activo; ?>">
+                                <?php echo esc_html(get_the_title()); // Muestra el título del post ?>
+                            </a>
+                        </li>
+                        <?php
+                        $i++; // Incrementa el contador
+                    endwhile;
+                    ?>
+                    <li>
+                            <a href="https://sagardoy.neuronalcode.io/contacto/" data-id="01" class="btn-link">
+                                <?php _e('Internacional', 'sagardoy'); ?>
+                            </a>
+                    </li>
+
+                    <?php
+                    wp_reset_postdata(); // Resetea la consulta
+                else :
+                    echo '<li>No hay sedes disponibles.</li>';
+                endif;
+                ?>
             </ul>
+
             <div class="info">
               <a href="mailto:<?php echo $email;?>" class="btn-email"><?php echo $email;?></a><br>
               <a href="tel:<?php echo $telefono; ?>" class="btn-tel"><?php echo $telefono;?></a>
@@ -38,36 +77,30 @@ $telefono = get_field('telefono_general', 'option');
             <div class="row">
               <div class="col-12">
                 <div class="imagenes-holder">
-                    <?php 
-                $i=1;
-                foreach ($sedes as $sede) {
-                    if($i==1){$activo='show';}else{$activo='';}
-                    echo '<div class="imagen-holder '.$activo.'" id="img-0'.$i.'">
-                    <div class="titulo">'.htmlspecialchars($sede['titulo_derecha']).'</div>
-                    <img src="'.htmlspecialchars($sede['imagen']).'" class="img-fluid" />
-                  </div>';
-                    $i++;
-                }?>
+                    <div class="imagen-holder show" id="img-01">
+                    <div class="titulo"><?php echo $titulo_derecha;?></div>
+                    <img src="<?php echo $imagen_url; ?>" class="img-fluid" />
+                  </div>
+                </div>
+                <div class="imagenes-holder">
+                    <div class="imagen-holder" id="img-02">
+                    <div class="titulo"><?php echo $titulo_derecha;?></div>
+                    <img src="<?php echo $imagen_url; ?>" class="img-fluid" />
+                  </div>
                 </div>
               </div>
             </div>
             <div class="row">
               <div class="col-12 col-sm-12 col-lg-5">
                 <div class="infos-holder">
-                <?php 
-                $i=1;
-                foreach ($sedes as $sede) {
-                    if($i==1){$activo='show';}else{$activo='';}
-                    echo '<div class="info-holder '.$activo.'" id="info-0'.$i.'">
-                    <div class="texto">'.htmlspecialchars($sede['descripcion']).'</div>
-                    <div class="info">
-                      <a href="#" class="btn-direccion">'.htmlspecialchars($sede['direccion']).'</a><br><br>
-                      <a href="tel:'.htmlspecialchars($sede['telefono']).'">'.htmlspecialchars($sede['telefono']).'</a><br>
-                      <a href="mailto:'.htmlspecialchars($sede['email']).'" class="btn-email">'.htmlspecialchars($sede['email']).'</a>
+                    <div class="info-holder show" id="info-01">
+                        <div class="texto"><?php echo $descripcion;?></div>
+                        <div class="info">
+                        <a href="#" class="btn-direccion"><?php echo $direccion;?></a><br><br>
+                        <a href="tel:<?php echo $telefono;?>"><?php echo $telefono;?></a><br>
+                        <a href="mailto:<?php echo $email;?>" class="btn-email"><?php echo $email;?></a>
+                        </div>
                     </div>
-                  </div>';
-                    $i++;
-                }?>
 
                 </div>
               </div>
@@ -93,36 +126,52 @@ $telefono = get_field('telefono_general', 'option');
             <div class="subtitulo"><?php _e('Nuestras Sedes', 'sagardoy'); ?></div>
           </div>
           <ul class="lista">
-             <?php 
-                $i=1;
-                foreach ($sedes as $sede) {
-                    if($i==1){$activo='activo';}else{$activo='';}
-                    echo '<li><a href="#" data-id="0'.$i.'" class="btn-link '.$activo.'">'.htmlspecialchars($sede['nombre']).'</a></li>';
-                    $i++;
-                }?>
+             <?php
+                // Realiza una consulta personalizada para obtener los posts del custom post type 'sedes'
+                $args = array(
+                    'post_type' => 'sedes', // Nombre del custom post type
+                    'posts_per_page' => -1, // Trae todos los posts
+                    'post_status' => 'publish', // Solo los publicados
+                );
+                $current_i = get_the_ID();
+                $query = new WP_Query($args);
+                $i = 1; // Contador para identificar el índice
+
+                if ($query->have_posts()) :
+                    while ($query->have_posts()) : $query->the_post();
+                        $activo = (get_the_ID() == $current_i) ? 'activo' : '';
+                        ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>" data-id="01" class="btn-link <?php echo $activo; ?>">
+                                <?php echo esc_html(get_the_title()); // Muestra el título del post ?>
+                            </a>
+                        </li>
+                        <?php
+                        $i++; // Incrementa el contador
+                    endwhile;
+                    wp_reset_postdata(); // Resetea la consulta
+                else :
+                    echo '<li>No hay sedes disponibles.</li>';
+                endif;
+                ?>
           </ul>
         </div>
         <div class="holder">
           <div class="container">
             <div class="datas">
 
-            <?php 
-                $i=1;
-                foreach ($sedes as $sede) {
-                    if($i==1){$activo='show';}else{$activo='';}
-                    echo '
-                  <div class="data '.$activo.'" id="data-0'.$i.'">
-                <div class="titulo"><a href="#" class="btn-volver"></a>'.htmlspecialchars($sede['nombre']).'</div>
-                <div class="texto">'.htmlspecialchars($sede['descripcion']).'</div>
+
+
+
+             <div class="data show" id="data-01">
+                <div class="titulo"><a href="#" class="btn-volver"></a><?php the_title();?></div>
+                <div class="texto"><?php echo $descripcion;?></div>
                 <div class="info">
-                  <a href="#" class="btn-direccion">'.htmlspecialchars($sede['direccion']).'</a><br><br>
-                  <a href="tel:'.htmlspecialchars($sede['telefono']).'">'.htmlspecialchars($sede['telefono']).'</a><br>
-                  <a href="mailto:'.htmlspecialchars($sede['email']).'" class="btn-email">'.htmlspecialchars($sede['email']).'</a>
+                  <a href="#" class="btn-direccion"><?php echo $direccion;?></a><br><br>
+                  <a href="tel:<?php echo $telefono;?>"><?php echo $telefono;?></a><br>
+                  <a href="mailto:<?php echo $email;?>" class="btn-email"><?php echo $email;?></a>
                 </div>
-              </div>';
-                    $i++;
-                }
-                ?>
+              </div>
             </div>
 
             <?php echo do_shortcode('[contact-form-7 id="55dd754" title="Contacto sedes movil"]');?>
