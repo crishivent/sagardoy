74dd822fddeb364cc651c6a9dd54534de6fca41c
Cristo <cristo.ruiz@hivent.es>
2024-12-09 12:58:48 +0100

    sedes vinculadas

--- sagardoy/single-equipo.php
+++ sagardoy/single-equipo.php
@@ -12,9 +12,10 @@ $idiomas= get_field('idiomas');
 $email= get_field('email');
 $telefono= get_field('telefono');
 $imagen_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
- $sede = get_field('sedes');
+$sede = get_field('sedes');
 
-
+$post = $sede[0];
+    
 
 ?>
 
@@ -35,7 +36,7 @@ $imagen_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
               </div>
               <div class="col-12 col-sm-4 col-md-4">
                 <div class="titulo-top"><?php _e('SEDES', 'sagardoy'); ?></div>
-                <div class="texto"><?php echo $sede;?></div>
+                <div class="texto"><?php echo esc_html($post->post_title);?></div>
               </div>
               <div class="col-12 col-sm-4 col-md-4">
                 <div class="titulo-top"><?php _e('IDIOMAS', 'sagardoy'); ?></div>
