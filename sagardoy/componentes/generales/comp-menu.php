9751992c1ea70477f347d44940e2994d938a825b
Cristo <cristo.ruiz@hivent.es>
2024-12-10 19:40:48 +0100

    submenu equipo

--- sagardoy/componentes/generales/comp-menu.php
+++ sagardoy/componentes/generales/comp-menu.php
@@ -5,11 +5,11 @@
  
  <nav class="navbar navbar-expand-lg">
     <div class="menu-content">
-      <a class="navbar-brand d-block d-lg-none" href="#"><img src="<?php echo $logomenu; ?>" class="logo" alt="" /></a>
+      <a class="navbar-brand d-block d-lg-none" href="/"><img src="<?php echo $logomenu; ?>" class="logo" alt="" /></a>
       <div id="main-nav" class="collapse navbar-collapse">
         <div class="navbar-nav">
           <div class="col-left">
-            <a class="navbar-brand" href="#"><img src="<?php echo $logomenu; ?>" class="logo" alt="" /></a>
+            <a class="navbar-brand" href="/"><img src="<?php echo $logomenu; ?>" class="logo" alt="" /></a>
           </div>
           <div class="col-right">
 
@@ -32,7 +32,117 @@
                             echo '<li class="btn-link_ '.$btnn.'"><a href="' . esc_url($item->url) . '">' . esc_html($item->title) . '</a>';
 
                             if ($item->title=='equipo') {
-                              get_template_part('componentes/generales/comp','submenuequipo');
+                              ?>
+                              <!-- Desplegable equipo -->
+                               <div class="submenu equipo">
+                                  <div class="row">
+                                      <div class="col-4">
+                                          <div class="titulo">Equipo</div>
+                                          <div class="texto">
+                                              Facilisi vitae suspendisse eget dui donec, a quisque quis in quis integer volutpat. In tellus commodo.
+                                          </div>
+                                      </div>
+                                      <div class="col-2"></div>
+                                      <div class="col-6">
+                                          <div class="form">
+                                              <form id="search-form" method="GET" action="https://sagardoy.neuronalcode.io/equipo/">
+                                                  <div class="row">
+                                                      <div class="col-12">
+                                                          <div class="subtitulo">Buscar abogado</div>
+                                                          <div class="input-email">
+                                                              <input 
+                                                                  type="text" 
+                                                                  name="_buscar" 
+                                                                  class="input" 
+                                                                  value="" 
+                                                                  placeholder="Buscar" />
+                                                              <a href="#" class="btn-lupa"></a>
+                                                          </div>
+                                                      </div>
+                                                  </div>
+                                                  <div class="row">
+                                                      <div class="col-4">
+                                                          <div class="select">
+                                                              <select name="_cargos" id="cargos-select">
+                                                                  <option value="" disabled selected>Cargo</option>
+                                                                  <?php
+                                                                  global $wpdb;
+                                                                  $meta_key = 'cargo';
+                                                                  $results = $wpdb->get_col(
+                                                                      $wpdb->prepare(
+                                                                          "SELECT DISTINCT meta_value 
+                                                                          FROM {$wpdb->postmeta} 
+                                                                          WHERE meta_key = %s 
+                                                                          AND meta_value != ''", 
+                                                                          $meta_key
+                                                                      )
+                                                                  );
+                                                                  if (!empty($results)) {
+                                                                      foreach ($results as $cargo) {
+                                                                          echo '<option value="' . esc_attr($cargo) . '">' . esc_html($cargo) . '</option>';
+                                                                      }
+                                                                  } else {
+                                                                      echo '<option value="0" disabled>No hay cargos disponibles</option>';
+                                                                  }
+                                                                  ?>
+                                                              </select>
+                                                          </div>
+                                                      </div>
+                                                      <div class="col-4">
+                                                          <div class="select">
+                                                              <select name="_sedes" id="sedes-select">
+                                                                  <option value="" disabled selected>Sede</option>
+                                                                  <?php
+                                                                  $args = array(
+                                                                      'post_type'      => 'sede',
+                                                                      'posts_per_page' => -1,
+                                                                      'post_status'    => 'publish',
+                                                                      'orderby'        => 'title',
+                                                                      'order'          => 'ASC',
+                                                                  );
+
+                                                                  $sede_query = new WP_Query($args);
+                                                                  if ($sede_query->have_posts()) {
+                                                                      while ($sede_query->have_posts()) {
+                                                                          $sede_query->the_post();
+                                                                          echo '<option value="' . esc_attr(get_the_ID()) . '">' . esc_html(get_the_title()) . '</option>';
+                                                                      }
+                                                                      wp_reset_postdata();
+                                                                  } else {
+                                                                      echo '<option value="0" disabled>No hay sedes disponibles</option>';
+                                                                  }
+                                                                  ?>
+                                                              </select>
+                                                          </div>
+                                                      </div>
+                                                      <div class="col-4">
+                                                          <button type="submit" class="btn-buscar">Buscar</button>
+                                                      </div>
+                                                  </div>
+                                              </form>
+                                          </div>
+                                          <ul class="btns">
+                                              <li><a href="#">Conoce a nuestro equipo</a></li>
+                                              <li><a href="#">Únete al equipo</a></li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+
+                              <script>
+                                  document.getElementById('search-form').addEventListener('submit', function (e) {
+                                      e.preventDefault();
+                                      const form = e.target;
+                                      const action = form.getAttribute('action');
+                                      const params = new URLSearchParams(new FormData(form));
+                                      const fullUrl = `${action}?${params.toString()}#equiporesult`;
+                                      window.location.href = fullUrl;
+                                  });
+                              </script>
+                              <!-- Fin Desplegable equipo -->
+
+
+                              <?php
                             }
 
                             if ($item->title=='actualidad') {
@@ -62,10 +172,10 @@
                  $menu_name = 'main';
                     if ($menu_name) {
                         $menu_items = wp_get_nav_menu_items($menu_name);
-                        if ($item->title=='equipo') {
+                        if ($item->title=='Equipo') {
                               $btnn='btn-equipo';
                           }
-                          if ($item->title=='actualidad') {
+                          if ($item->title=='Actualidad') {
                               $btnn='btn-actualidad';
                           }
                         
--- sagardoy/componentes/generales/comp-submenuequipo.php
+++ sagardoy/componentes/generales/comp-submenuequipo.php
@@ -1,55 +1,105 @@
 <div class="submenu equipo">
-                  <div class="row">
-                    <div class="col-4">
-                      <div class="titulo">Equipo</div>
-                      <div class="texto">Facilisi vitae suspendisse eget dui donec, a quisque quis in quis integer volutpat. In tellus commodo.</div>
-                    </div>
-                    <div class="col-2"></div>
-                    <div class="col-6">
-                      <div class="form">
-                        <div class="row">
-                          <div class="col-12">
+    <div class="row">
+        <div class="col-4">
+            <div class="titulo">Equipo</div>
+            <div class="texto">
+                Facilisi vitae suspendisse eget dui donec, a quisque quis in quis integer volutpat. In tellus commodo.
+            </div>
+        </div>
+        <div class="col-2"></div>
+        <div class="col-6">
+            <div class="form">
+                <form id="search-form" method="GET" action="https://sagardoy.neuronalcode.io/equipo/">
+                    <div class="row">
+                        <div class="col-12">
                             <div class="subtitulo">Buscar abogado</div>
                             <div class="input-email">
-                              <input class="input" value="" placeholder="Buscar"></input>
-                              <a href="#" class="btn-lupa"></a>
+                                <input 
+                                    type="text" 
+                                    name="_buscar" 
+                                    class="input" 
+                                    value="" 
+                                    placeholder="Buscar" />
+                                <a href="#" class="btn-lupa"></a>
                             </div>
-                          </div>
                         </div>
-                        <div class="row">
-                          <div class="col-4">
-                            <div class="select">
-                              <select id="standard-select">
-                                <option value="0" disabled selected>Cargo</option>
-                                <option value="1">Opción 1</option>
-                                <option value="2">Opción 2</option>
-                                <option value="3">Opción 3</option>
-                                <option value="4">Opción 4</option>
-                                <option value="5">Opción 5</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div class="col-4">
-                            <div class="select">
-                              <select id="standard-select">
-                                <option value="0" disabled selected>Sede</option>
-                                <option value="1">Opción 1</option>
-                                <option value="2">Opción 2</option>
-                                <option value="3">Opción 3</option>
-                                <option value="4">Opción 4</option>
-                                <option value="5">Opción 5</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div class="col-4">
-                            <a href="#" class="btn-buscar">Buscar</a>
-                          </div>
-                        </div>
-                      </div>
-                      <ul class="btns">
-                        <li><a href="#">Conoce a nuestro equipo</a></li>
-                        <li><a href="#">Únete al equipo</a></li>
-                      </ul>
                     </div>
-                  </div>
-                </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="select">
+                                <select name="_cargos" id="cargos-select">
+                                    <option value="" disabled selected>Cargo</option>
+                                    <?php
+                                    global $wpdb;
+                                    $meta_key = 'cargo';
+                                    $results = $wpdb->get_col(
+                                        $wpdb->prepare(
+                                            "SELECT DISTINCT meta_value 
+                                            FROM {$wpdb->postmeta} 
+                                            WHERE meta_key = %s 
+                                            AND meta_value != ''", 
+                                            $meta_key
+                                        )
+                                    );
+                                    if (!empty($results)) {
+                                        foreach ($results as $cargo) {
+                                            echo '<option value="' . esc_attr($cargo) . '">' . esc_html($cargo) . '</option>';
+                                        }
+                                    } else {
+                                        echo '<option value="0" disabled>No hay cargos disponibles</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="select">
+                                <select name="_sedes" id="sedes-select">
+                                    <option value="" disabled selected>Sede</option>
+                                    <?php
+                                    $args = array(
+                                        'post_type'      => 'sede',
+                                        'posts_per_page' => -1,
+                                        'post_status'    => 'publish',
+                                        'orderby'        => 'title',
+                                        'order'          => 'ASC',
+                                    );
+
+                                    $sede_query = new WP_Query($args);
+                                    if ($sede_query->have_posts()) {
+                                        while ($sede_query->have_posts()) {
+                                            $sede_query->the_post();
+                                            echo '<option value="' . esc_attr(get_the_ID()) . '">' . esc_html(get_the_title()) . '</option>';
+                                        }
+                                        wp_reset_postdata();
+                                    } else {
+                                        echo '<option value="0" disabled>No hay sedes disponibles</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn-buscar">Buscar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <ul class="btns">
+                <li><a href="#">Conoce a nuestro equipo</a></li>
+                <li><a href="#">Únete al equipo</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<script>
+    document.getElementById('search-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        const form = e.target;
+        const action = form.getAttribute('action');
+        const params = new URLSearchParams(new FormData(form));
+        const fullUrl = `${action}?${params.toString()}#equiporesult`;
+        window.location.href = fullUrl;
+    });
+</script>
