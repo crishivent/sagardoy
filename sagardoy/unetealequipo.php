f1cfa8be616b6ae4f5ee3e36098c8b5c2cd6775e
Cristo <cristo.ruiz@hivent.es>
2024-12-22 14:36:59 +0100

    unete al equipo

---
+++ sagardoy/unetealequipo.php
@@ -0,0 +1,142 @@
+<?php
+/*
+Template Name: Unete al equipo
+*/
+
+get_header();
+
+
+ ?>
+
+
+<!-- Cabecera -->
+  <section class="section cabecera-interna cabecera">
+    <div class="holder">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-7 col-md-7 border-left">
+            <div class="label">Únete al equipo</div>
+            <div class="titulo">Buscamos talento y profesionalidad</div>
+          </div>
+          <div class="col-12 col-sm-5 col-md-5"></div>
+        </div>
+      </div>
+    </div>
+    <img src="images/img-cabecera-unete-al-equipo.jpg" class="img-fluid" alt="" />
+  </section>
+
+<!-- Bloque 1 -->
+   <section class="modulo-24 pt-68 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-lg-7">
+          <div class="texto">Quam dui montes quam ultrices aliquet mollis ornare amet. Viverra phasellus sit integer proin faucibus. Ut nisl dictumst odio neque. Odio feugiat scelerisque non ac. Urna netus pellentesque nulla elementum. Turpis dictum vulputate bibendum velit sagittis. Purus nulla.</div>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-5"></Div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-lg-4">
+          <h2 class="titulo">Da un impulso a tu carrera profesional</h2>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-1"></div>
+        <div class="col-12 col-sm-12 col-lg-7">
+          <img src="images/img-unete-al-equipo-1.jpg" class="img-fluid" alt="" />
+          <ul class="lista">
+            <li>Sagittis egestas quis convallis fringilla mauris lacus euismod tellus.</li>
+            <li>Nec tristique nulla rutrum augue quam maecenas pharetra fames.</li>
+            <li>Lacus placerat viverra sed ipsum elementum pharetra. Gravida porttitor dolor est ut dignissim consequat cras. Pellentesque lorem consectetur quam elit id. Elementum at id arcu eget gravida lectus sed. Eget avida porttitor dolor est ut dignissim consequat.</li>
+            <li>Nec tristique nulla rutrum augue quam.</li>
+            <li>Elementum at id arcu eget gravida lectus sed.</li>
+          </ul>
+          <a href="#" class="btn-link">únete al equipo</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<!-- Bloque 2 con foto -->
+
+  <section class="modulo-25 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-lg-5">
+          <div class="texto">“Id ac purus odio molestie semper arcu. Nam enim nulla et dolor at et feugiat at. Suspendisse aliquam phasellus donec non. Quam sagittis ornare sollicitudin lorem ornare vel. Mi turpis fusce vitae luctus purus natoque nunc ultricies nunc. Lacinia ut orci tempor pretium tortor.”</div>
+          <div class="nombre">Nombre y Apellidos</div>
+          <div class="cargo">Cargo</div>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-1"></div>
+        <div class="col-12 col-sm-12 col-lg-6">
+          <div class="img-holder"><img src="images/img-unete-al-equipo-2.jpg" class="img-fluid" alt="" /></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+<!-- Formulario -->
+  <section class="modulo-26 pt-130 pb-130">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-sm-12 col-lg-3"></div>
+        <div class="col-12 col-sm-12 col-lg-6">
+          <h2 class="titulo">Empieza tu futuro profesional</h2>
+          <form>
+            <div class="row">
+              <div class="col-12 col-sm-12 col-lg-6">
+                <div class="input-holder"><input class="input" value="" placeholder="Nombre"></input></div>
+              </div>
+              <div class="col-12 col-sm-12 col-lg-6">
+                <div class="input-holder"><input class="input" value="" placeholder="Apellidos"></input></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="input-holder"><input class="input" value="" placeholder="Email"></input></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 col-sm-12 col-lg-6">
+                <div class="input-holder"><input class="input" value="" placeholder="Empresa"></input></div>
+              </div>
+              <div class="col-12 col-sm-12 col-lg-6">
+                <div class="input-holder"><input class="input" value="" placeholder="Cargo"></input></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="select">
+                  <select id="standard-select">
+                    <option value="0" disabled selected>Seleccionar departamento</option>
+                    <option value="1">Opción 1</option>
+                    <option value="2">Opción 2</option>
+                    <option value="3">Opción 3</option>
+                    <option value="4">Opción 4</option>
+                    <option value="5">Opción 5</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="politica">
+                  <label class="checkbox-holder">
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                  </label>
+                  <div class="label">Enviando este formulario estoy aceptando la <a href="#">política de privacidad</a></div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <a href="#" class="btn-enviar">Enviar</a>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-3"></div>
+      </div>
+    </div>
+  </section>
