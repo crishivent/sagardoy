150ea0f95e12fcabf39bf32f45f2269ed418c0ca
Cristo <cristo.ruiz@hivent.es>
2025-01-17 13:16:18 +0100

    premios home

--- sagardoy/componentes/home/comp-premios.php
+++ sagardoy/componentes/home/comp-premios.php
@@ -3,23 +3,40 @@ $titulopremios=get_query_var("titulo_premios");
 $premios=get_query_var("premios");
 
 ?>   
-   
-   
-   <section class="modulo-02 pt-130 pb-130">
+
+
+<section id="greconocimiento" class="modulo-27 pt-130 pb-65">
     <div class="container">
       <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="texto"><?php echo $titulopremios; ?></h2>
+        <div class="col-12 col-sm-12 col-lg-9">
+          <h2 class="titulo"><?php echo $titulopremios; ?></h2>
+        </div>
+        <div class="col-12 col-sm-12 col-lg-3 d-flex align-items-end justify-content-end">
+          <div id="btns_slider_01" class="btns-slider">
+            <a href="#" class="icons ico-flecha-left btn-prev"></a>
+            <a href="#" class="icons ico-flecha-right btn-next"></a>
+          </div>
         </div>
       </div>
-      <div class="slider-logos">
+      <div id="slider_01" class="slider-premios">
         <div class="slick-slider" data-sizes="100vw">
-           <?php 
+
+        <?php 
               foreach ($premios as $premio) {
-                $imagen = $premio['premio'];
-                  echo '<div><img src="'.$imagen.'" class="img-fluid" alt=""></div>';
+                $imagen = $premio['premios']['premio'];
+                $texto = $premio['premios']['texto_premio'];
+                $enlacepremios= $premio['premios']['enlace_premios'];
+                  echo '<div>
+                          <a href="'.$enlacepremios.'">
+                            <div class="line-border">
+                              <img src="'.$imagen.'" class="img-fluid" alt="">
+                              <div class="texto">'.$texto.'</div>
+                            </div>
+                          </a>
+                        </div>';
               }
         ?>
+
         </div>
       </div>
     </div>
